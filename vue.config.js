e8e4c9e3cafef2c9f00cf2496ac8cc48986085a8
abojkov <ace.bojkov11@gmail.com>
2023-02-14 17:30:44 +0100

    Changes

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,9 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    disableHostCheck: true,
-    host: '0.0.0.0'
+    allowedHosts: [
+      "railway.app",
+      "raleway-vuejs-frontend-production.up.railway.app/"
+    ]
   }
 })
