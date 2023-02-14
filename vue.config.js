07d7b1678731ce8deddd03270989937d8fcce69b
abojkov <ace.bojkov11@gmail.com>
2023-02-14 17:33:07 +0100

    Changes

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     allowedHosts: [
+      "0.0.0.0",
       "railway.app",
       "raleway-vuejs-frontend-production.up.railway.app/"
     ]
